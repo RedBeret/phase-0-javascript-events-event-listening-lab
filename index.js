df0ca3575d5256c4a90205ef4f23611a3deaf930
Steven Espinoza <espisteven5@gmail.com>
2023-11-04 12:34:21 -0700

    added event listener

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
-}
+    const clickMe = document.getElementById('button');
+    clickMe.addEventListener('click', e => {
+        alert('I was clicked!');
+        // console.log("I was clicked!");
+    });
+};
+addingEventListener();
